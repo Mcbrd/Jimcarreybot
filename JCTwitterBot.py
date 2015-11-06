4b5e7dedc47429cf0b784d890e0d83c7e0528c73
CHilke1 <chad.hilke@gmail.com>
2015-11-06 11:01:12 -0600

    Revised Twitter bot 2.0

--- JCTwitterBot.py
+++ JCTwitterBot.py
@@ -1,45 +1,79 @@
-# Comments go here.
-
+# comments
 import tweepy, time, sys
 from TwitterBotKeys import keys
 from datetime import datetime, timedelta
 from random import randint
+from os import listdir
+from os.path import isfile, join
+import os
 
-CONSUMER_KEY = keys['consumer_key']
-CONSUMER_SECRET = keys['consumer_secret']
-ACCESS_TOKEN = keys['access_token']
-ACCESS_TOKEN_SECRET = keys['access_token_secret']
-
-argfile = str("liners.txt")
-
-try:
-	filename = open(argfile, "r")
-except IOError:
-	print("cannot open", argfile)
+class Twitbot:
+	def __init__(self, keys):  
+		CONSUMER_KEY = keys['consumer_key']
+		CONSUMER_SECRET = keys['consumer_secret']
+		ACCESS_TOKEN = keys['access_token']
+		ACCESS_TOKEN_SECRET = keys['access_token_secret']
+		auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
+		#auth.secure = True
+		auth.set_access_token(ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
+		self.api = tweepy.API(auth)
+		user = self.api.me()
+		print('Name: ' + user.name)
+		print('Location: ' + user.location)
+		print('Friends: ' + str(user.friends_count))
 	
-f = filename.readlines()
-filename.close()
+	def tweet(self, f):       
+		for s in f:
+			self.api.update_status(status = s)
+			print(s)
+			
+	def updateStatusGIF(self):
+		mypath = ".\gifs"
+		onlyfiles = [ f for f in listdir(mypath) if isfile(join(mypath,f)) ]
+		for file in onlyfiles:
+			relpath = mypath + "\\" + file
+			statusGIF = os.path.abspath(relpath)
+			print(statusGIF)
+			self.api.update_with_media(filename=statusGIF, status="Smokin'!")
 
-auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
-auth.set_access_token(ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
+def getLines(filename):			
+	argfile = str(filename)
+	try:
+		file = open(argfile, "r")
+	except IOError:
+		print("cannot open", argfile)
+	f = file.readlines()
+	file.close()
+	return f
+			
+def main():
+	JCBot = Twitbot(keys)
+	f = getLines("liners.txt")
+	#twitter.tweet(f)
+	JCBot.updateStatusGIF()
+	replyText = "Ace is on the case!"
+	for tweet in JCBot.api.search(q="lost pet", count=1):
+		sn = tweet.user.screen_name
+		m = "@{0}, {1}".format(sn, replyText) 
+		id = str(tweet.id)
+		print(replyText)
+		print(tweet.user.screen_name)
+		print(id)
+		x = JCBot.api.update_status(status=m, in_reply_to_status_id=id) 
 
-try:
-	redirect_url = auth.get_authorization_url()
-except tweepy.TweepError:
-	print("Error! Failed to get request token.")
+#	replyText = "Laces out Dan!"
+#	for tweet in api.search(q="Dolphin", count=1):
+#		sn = tweet.user.screen_name
+#		m = "@{0}, {1}".format(sn, replyText) 
+#		print(replyText)
+#		print(tweet.user.screen_name)
+#		s = api.update_status(status=m, in_reply_to_status_id = tweet.id)
+#	replyText = "I'm in psychoville, and Finkle's the mayor!"
+#	for tweet in api.search(q="crazy", count=1):
+#		sn = tweet.user.screen_name
+#		m = "@{0}, {1}".format(sn, replyText) 
+#		print(replyText)
+#		print(tweet.user.screen_name)
+#		s = api.update_status(status=m, in_reply_to_status_id = tweet.id)
 	
-api = tweepy.API(auth)
-
-user = api.me()
- 
-print('Name: ' + user.name)
-print('Location: ' + user.location)
-print('Friends: ' + str(user.friends_count))
-
-replyText = "Ace is on the case!"
-for tweet in api.search(q="lost pet", count=2):
-	sn = tweet.user.screen_name
-	m = "@{0}, {1}".format(sn, replyText) 
-	print(replyText)
-	print(tweet.user.screen_name)
-	s = api.update_status(status=m, in_reply_to_status_id = tweet.id)
+if __name__ == "__main__": main() 
